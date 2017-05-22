7496b487c2c0d77ab89a2530de0110ca596a6ce0
Felipe de Vargas <felipevar26@gmail.com>
2017-05-22 10:35:55 -0300

    Update weather-based.R

--- weather-based.R
+++ weather-based.R
@@ -14,9 +14,9 @@ teste = read_delim('LONDRINA_2004.csv',
                                       'chcp', 'dif_adj_lag3', 'diaFavInf')))
 
 annDate = teste %>%
-  filter(variable %in% c('diaFavInf', 'chcp', 'dif_adj_lag3')) %>%
+  filter(variable %in% c('diaFavInf', 'chcp','dif_adj_lag3')) %>%
   spread(variable, value) %>%
-  filter(chcp >= 30, diaFavInf == 1, dif_adj_lag3>=0.4) %>%
+  filter(chcp >= 30, diaFavInf == 1,dif_adj_lag3>=0.4) %>%
   select(dd) %>% as.data.frame
 
 dGraph = teste %>% filter(variable != 'diaFavInf')
@@ -29,18 +29,20 @@ varNames = c(
   dif_adj_lag3 = "Spore\nCloud"
 )
 
-ggplot(dGraph,
+p<-ggplot(dGraph,
        aes(x = dd, y = value, ymin = 0, ymax = value)) +
   facet_grid(variable~., scales = "free",
              as.table = FALSE,
              labeller = labeller( variable = varNames, .multi_line = TRUE )) +
   geom_step(data = teste %>%
               filter(variable %in% c( 'chcp'))) +
+  #                  geom_hline(yintercept = 30)+
   geom_bar(data = teste %>%
-             filter(variable %in% c('chuva')),stat= "identity") +
+              filter(variable %in% c('chuva')),stat= "identity") +
   geom_line(data = teste %>%
               filter(variable %in% c('ur', 'temp', 'dif_adj_lag3'))) +
   annotate("rect", xmin=annDate$dd, xmax = annDate$dd+1,
            ymin=-Inf, ymax=Inf, alpha=0.2, fill="red") +
-  theme_bw() + xlab("Date") + ylab("Values") +
-  ggsave(filename = 'Figure3.png', width = 8, height = 10, dpi = 600)
+  theme_bw() + xlab("Date") + ylab("Values")+
+  scale_x_date(date_breaks = "1 week",labels = date_format("%d-%m"))
+ggsave(filename = "Figure 3.png",plot=p, width=8, height=10, dpi=600 )
